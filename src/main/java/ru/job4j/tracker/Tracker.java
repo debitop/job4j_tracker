82defca04431ae60f1b4d0507ede01f25171cbd2
Aleksandr Kochkurkin <info@debitop.com>
2022-03-03 20:46:52 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int temp = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];
+            if (name != null && key.equals(name.getName())) {
+                rsl[temp] = name;
+                temp++;
+            }
+        }
+        return Arrays.copyOf(rsl, temp);
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int temp = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];
+            if (name != null) {
+                rsl[temp] = name;
+                temp++;
+            }
+        }
+        return Arrays.copyOf(rsl, temp);
     }
 }
