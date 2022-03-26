9f12f967505783cf4a279ef0674694cc1e4c3706
Aleksandr Kochkurkin <info@debitop.com>
2022-03-26 19:05:10 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException("ElementNotFound");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"test1"};
+        String key = "test2";
+        try {
+            indexOf(value, key);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
