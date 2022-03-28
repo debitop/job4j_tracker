9c0be0a99117528fbdc87dabb665e7f2bdee16c0
Aleksandr Kochkurkin <info@debitop.com>
2022-03-29 00:18:45 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,35 @@
+package ru.job4j.ooa;
+
+public class Airbus {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        int engine = name.equals("А380") ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + engine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
