500683e86407a36c0d4b13df1af83b854256a0c3
Aleksandr Kochkurkin <info@debitop.com>
2022-03-28 20:11:29 +0300

    2.1. Тест на ValidateInput [#311887 #281148]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -44,10 +44,12 @@ public class ValidateInputTest {
     public void whenDoubleCorrectInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"1", "1"}
+                new String[]{"1", "2"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected, is(1));
+        int selectedOne = input.askInt("Enter menu:");
+        assertThat(selectedOne, is(1));
+        int selectedTwo = input.askInt("Enter menu:");
+        assertThat(selectedTwo, is(2));
     }
 }
