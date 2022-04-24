585720d3e6c76a23f8e9ee3955b502d6534f5827
Aleksandr Kochkurkin <info@debitop.com>
2022-04-24 19:12:08 +0300

    1. Паспорт и жители [#212656 #292889]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,15 +8,10 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean rsl = true;
-        for (String key : citizens.keySet()) {
-            if (key.contains(citizen.getPassport())) {
-                rsl = false;
-                break;
-            }
-        }
-        if (rsl) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
         }
         return rsl;
     }
