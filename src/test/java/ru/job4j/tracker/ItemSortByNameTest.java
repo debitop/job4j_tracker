7429789e7d7518c73710d0432f8d0b9f8fc1e451
Aleksandr Kochkurkin <info@debitop.com>
2022-04-25 10:33:25 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemSortByNameTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ItemSortByNameTest {
+
+    @Test
+    public void itemSortByNameAcs() {
+        List<Item> items = Arrays.asList(new Item("one"), new Item("two"), new Item("five"));
+        Collections.sort(items, new ItemAscByName());
+        List<Item> expected = Arrays.asList(new Item("five"), new Item("one"), new Item("two"));
+        boolean rsl = true;
+        for (int i = 0; i < items.size(); i++) {
+            if (!items.get(i).getName().equals(expected.get(i).getName())) {
+                rsl = false;
+                break;
+            }
+        }
+        assertTrue(rsl);
+    }
+
+    @Test
+    public void itemSortByNameDesc() {
+        List<Item> items = Arrays.asList(new Item("one"), new Item("two"), new Item("five"));
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = Arrays.asList(new Item("two"), new Item("one"), new Item("five"));
+        boolean rsl = true;
+        for (int i = 0; i < items.size(); i++) {
+            if (!items.get(i).getName().equals(expected.get(i).getName())) {
+                rsl = false;
+                break;
+            }
+        }
+        assertTrue(rsl);
+    }
+
+
+
+}
