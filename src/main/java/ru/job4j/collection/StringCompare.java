2ad2b2293a5f6e5adbd46bdcf47a505cec9d805e
Aleksandr Kochkurkin <info@debitop.com>
2022-04-29 18:49:06 +0300

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int rsl = Integer.compare(left.length(), right.length());
+        int length = (rsl >= 0) ? right.length() : left.length();
+
+        for (int i = 0; i < length; i++) {
+            int charCompare = Character.compare(left.charAt(i), right.charAt(i));
+            if (charCompare != 0) {
+                rsl = charCompare;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
