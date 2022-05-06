40abf60304fe119bda0d26f0a7ea4f8539f96aab
Aleksandr Kochkurkin <info@debitop.com>
2022-05-06 11:40:58 +0300

    Упражнения 1.4.1. Lambda 1.1. Predicate. Проверить, что число положительное

---
+++ src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckPositive {
+
+    public static boolean check(int num) {
+        Predicate<Integer> pred = s -> s > 0;
+        return check(pred, num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
