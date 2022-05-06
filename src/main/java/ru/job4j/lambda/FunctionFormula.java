55778e3931b83e8fcd997c6061105caf4579e22d
Aleksandr Kochkurkin <info@debitop.com>
2022-05-06 12:00:33 +0300

    1.4.1. Lambda 2.3. Fuction. Рассчитать значение по формуле

---
+++ src/main/java/ru/job4j/lambda/FunctionFormula.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.function.Function;
+
+public class FunctionFormula {
+
+    public static double calculate(double x) {
+        Function<Double, Double> func;
+        return calculate(num -> num * num + 2 * num + 1, x);
+    }
+
+    public static double calculate(Function<Double, Double> y, double x) {
+        return y.apply(x);
+    }
+
+}
