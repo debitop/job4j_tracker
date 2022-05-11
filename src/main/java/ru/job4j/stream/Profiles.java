24dbf4afba116bdbf75b875cee534630c2eeed71
Aleksandr Kochkurkin <info@debitop.com>
2022-05-11 11:12:55 +0300

    2. Список адресов [#110062 #299666]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(a -> a.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
