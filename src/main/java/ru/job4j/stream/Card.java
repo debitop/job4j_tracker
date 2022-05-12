65499a4414edb270346f5b600f3dc876f466c764
Aleksandr Kochkurkin <info@debitop.com>
2022-05-12 07:58:40 +0300

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
+    }
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit1 -> Stream.of(Value.values())
+                        .map(value1 -> new Card(suit1, value1)))
+                .forEach(System.out::println);
+    }
+}
