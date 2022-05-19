748a69907f8bba7043223dfbf66c2aff46a98346
“Aleksandr <89263693878@mail.ru>
2022-05-19 09:05:41 +0300

    4. Optional в Stream API. [#279130]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -12,28 +12,18 @@ public class College {
     }
 
     public Optional<Student> findByAccount(String account) {
-        Optional<Student> rsl = Optional.empty();
-        for (Student s : students.keySet()) {
-            if (account.equals(s.getAccount())) {
-                rsl = Optional.of(s);
-                break;
-            }
-        }
-        return rsl;
+        return students.keySet()
+                .stream()
+                .filter(s -> s.getAccount().equals(account))
+                .findFirst();
     }
 
     public Optional<Subject> findBySubjectName(String account, String name) {
         Optional<Subject> rsl = Optional.empty();
-        Optional<Student> s = findByAccount(account);
-        if (s.isPresent()) {
-            Set<Subject> subjects = students.get(s.get());
-            for (Subject subj : subjects) {
-                if (name.equals(subj.getName())) {
-                    rsl = Optional.of(subj);
-                    break;
-                }
-            }
-        }
-        return rsl;
+        Optional<Student> a = findByAccount(account);
+        return a.map(student -> students.get(student)
+                .stream()
+                .filter(s -> s.getName().equals(name))
+                .findFirst()).orElse(rsl);
     }
 }
