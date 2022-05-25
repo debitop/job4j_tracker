09524efa2fd80c06f63661575061f671c3d1becc
“Aleksandr <89263693878@mail.ru>
2022-05-25 10:06:06 +0300

    1.4.2. Stream API 2.3. Группировка элементов и сумма. Метод summigtInt()

--- src/main/java/ru/job4j/lambda/OptionalOrlElse.java
+++ src/main/java/ru/job4j/lambda/OptionalOrlElse.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 public class OptionalOrlElse {
     public static Integer orElse(Optional<Integer> optional) {
-        // do refactor
         Integer value = -1;
         if (optional.isPresent()) {
             value = optional.get();
